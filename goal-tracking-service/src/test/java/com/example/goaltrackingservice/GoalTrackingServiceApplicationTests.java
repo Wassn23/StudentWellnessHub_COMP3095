4b713fa9b8d5721e6b8c4150df0da440456366c1
olympia15 <olympiamichelle15@gmail.com>
2025-11-12 01:22:08 -0500

    updateGoalTest implemented

--- goal-tracking-service/src/test/java/com/example/goaltrackingservice/GoalTrackingServiceApplicationTests.java
+++ goal-tracking-service/src/test/java/com/example/goaltrackingservice/GoalTrackingServiceApplicationTests.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.testcontainers.containers.MongoDBContainer;
 import org.hamcrest.Matchers;
 
+import java.time.LocalDate;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GoalTrackingServiceApplicationTests {
 
@@ -50,7 +52,7 @@ class GoalTrackingServiceApplicationTests {
                 .body(requestBody)
                 .contentType(ContentType.JSON)
                 .when()
-                .post("/api/goals/create")
+                .post("/api/goals/")
                 .then()
                 .log().all()
                 .statusCode(HttpStatus.CREATED.value())
@@ -60,6 +62,75 @@ class GoalTrackingServiceApplicationTests {
                 .body("targetDate", Matchers.equalTo(2025-06-18))
                 .body("status", Matchers.equalTo("in-progress"))
                 .body("category", Matchers.equalTo("exercise"));
+    }
+
+    private String createGoalAndReturnId(String title, String description, LocalDate targetDate, String status, String category) {
+
+        String requestBody = """
+                
+                    {
+                        "title" : "%s",
+                        "description" : "%s",
+                        "targetDate" : "%s",
+                        "status" : "%s",
+                        "category" : "%s"
+                    }
+                
+                """.formatted(title, description, targetDate.toString(), status, category);
+
+        return RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("api/goals/")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract()
+                .path("goalId");
+
+    }
+
+    @Test
+    void updateGoalTest(){
+
+        // original product
+        String id = createGoalAndReturnId("Stay Hydrated", "Drink at least 2L of water per day",
+                LocalDate.of(2025, 3, 20), "in-progress", "nutrition");
+
+        String requestBody = """
+                
+                    {
+                        "title" : "Stay Hydrated",
+                        "description" : "Drink at least 3L of water per day",
+                        "targetDate" : 2025-03-20,
+                        "status" : "in-progress",
+                        "category" : "nutrition"
+                    }
+               
+                """;
+
+        RestAssured.given()
+                .body(requestBody)
+                .contentType(ContentType.JSON)
+                .when()
+                .put("api/goals/{id}" + id)
+                .then()
+                .log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value())
+                .header("Location", Matchers.equalTo("http://localhost:" + port + "/api/goals/" + id));
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("api/goals/")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("find {it.id == '%s' }.title".formatted(id), Matchers.equalTo("Stay Hydrated"))
+                .body("find {it.id == '%s' }.description".formatted(id), Matchers.equalTo("Drink at least 3L of water per day"))
+                .body("find {it.id == '%s' }.targetDate".formatted(id), Matchers.equalTo(2025-03-20))
+                .body("find {it.id == '%s' }.status".formatted(id), Matchers.equalTo("in-progress"))
+                .body("find {it.id == '%s' }.category".formatted(id), Matchers.equalTo("nutrition"));
+
     }
 
 
