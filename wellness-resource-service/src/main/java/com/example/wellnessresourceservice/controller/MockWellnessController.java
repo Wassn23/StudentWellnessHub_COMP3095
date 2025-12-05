bd43e938394162b639e5198d8bc9a628acfd6cc5
Gabriel Aparicio <195454686+GabeAparicio@users.noreply.github.com>
2025-12-05 16:26:35 -0500

    Added API Gateway + Keycloak integration for Assignment 2

--- wellness-resource-service/src/main/java/com/example/wellnessresourceservice/controller/MockWellnessController.java
+++ wellness-resource-service/src/main/java/com/example/wellnessresourceservice/controller/MockWellnessController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/mock-resources")
+@RequestMapping("/api/wellness")
 public class MockWellnessController {
 
     @GetMapping("/category/{category}")
